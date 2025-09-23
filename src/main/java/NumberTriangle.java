e220a60a63ba76d175c93c3b9719daace94c0c86
Derek <Dekre.Lam@mail.utoronto.ca>
2025-09-22 22:10:53 -0400

    Implement retrieve(String path) in NumberTriangle

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -88,8 +88,18 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
-        return -1;
+        NumberTriangle curr = this;
+        for (int i = 0; i < path.length(); i++) {
+            char ch = path.charAt(i);
+            if (ch == 'l') {
+                curr = curr.left;   // assume well-formed path per spec
+            } else if (ch == 'r') {
+                curr = curr.right;
+            } else {
+                throw new IllegalArgumentException("Invalid char at " + i + ": " + ch);
+            }
+        }
+        return curr.root;
     }
 
     /** Read in the NumberTriangle structure from a file.
